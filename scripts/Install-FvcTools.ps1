a5c7705ffee11db0e427b46e690a0d108186e0b2
Boris Resnick <boris@flyver.city>
2025-01-27 16:44:28 +0200

    Added --update key to the install script

--- scripts/Install-FvcTools.ps1
+++ scripts/Install-FvcTools.ps1
@@ -1,6 +1,6 @@
- $token = $(aws codeartifact get-authorization-token --domain flyvercity --domain-owner 368281077578 --region eu-west-3 --query authorizationToken --output text)
+ $token = $(aws --profile flyvercity codeartifact get-authorization-token --domain flyvercity --domain-owner 368281077578 --region eu-west-3 --query authorizationToken --output text)
 if ($LASTEXITCODE -ne 0) { throw "Failed to get authorization token" }
 
 $repo = "https://aws:$token@flyvercity-368281077578.d.codeartifact.eu-west-3.amazonaws.com/pypi/common/simple/"
-pip install --index-url $repo fvctools
+pip install --index-url $repo fvctools --upgrade
 if ($LASTEXITCODE -ne 0) { throw "Failed to install fvctools" }
