d98a8fc9ead57a2571f1ef367d024c3b4d7bc7a7
Boris Resnick <boris.resnick@gmail.com>
2025-10-13 10:36:49 +0300

    feat(agentfly): add semicolon delimiter and origin field support

--- src/fvc/tools/df/xformats/agentfly.py
+++ src/fvc/tools/df/xformats/agentfly.py
@@ -6,11 +6,21 @@ import logging as lg
 from fvc.tools.df.utils import JsonlinesIO
 
 
+def module_help():
+    return '- use-semicolon: Use semicolon as delimiter'
+
+
 def convert_to_fvc(params, metadata, input_path: Path, output: JsonlinesIO):
     metadata.update({'content': 'flightlog', 'source': 'agentfly'})
     output.write(metadata)
+
+    if 'use-semicolon' in params.get('custom', []):
+        delimiter = ';'
+    else:
+        delimiter = ','
+
     with input_path.open('rt') as input:
-        reader = csv.DictReader(input)
+        reader = csv.DictReader(input, delimiter=delimiter)
 
         for row in reader:
             try:
@@ -26,6 +36,9 @@ def convert_to_fvc(params, metadata, input_path: Path, output: JsonlinesIO):
                     },
                     'sensor': row['source_id']
                 }
+
+                if origin := row.get('origin'):
+                    record['origin'] = origin
 
                 output.write(record)
 
